c485bf538fe3e1b75ba515a9ab5a3ffb3478be1f
Theo Kleinendorst <tkleinen@gmail.com>
2019-11-26 14:09:28 +0100

    updated for bro

--- leiden/leiden/urls.py
+++ leiden/leiden/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^data/', include('acacia.data.urls',namespace='acacia')),
     url(r'^net/', include('acacia.meetnet.urls',namespace='meetnet')),
+    url(r'^bro/', include('acacia.meetnet.bro.urls',namespace='bro')),
     url(r'^locs/',well_locations,name='locs'),
     url(r'^pop/(?P<pk>\d+)', PopupView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
